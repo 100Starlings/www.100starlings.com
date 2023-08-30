4eacc4f92d6ba91c8296ea3da8433927692d019a
mattmorvai <matt.morvai@100starlings.com>
2023-08-30 14:09:55 +0200

    small changes

---
+++ images/ImageUrls.js
@@ -0,0 +1,54 @@
+export const delivery =
+	"https://images.unsplash.com/photo-1582902281043-69c645f40cd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
+export const aviation =
+	"https://images.unsplash.com/photo-1503379230423-19c53f7e9a33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2373&q=80";
+export const electric_car =
+	"https://images.unsplash.com/photo-1628024963793-7823e403cf82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
+export const develpoers =
+	"https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80";
+export const avatar =
+	"https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80";
+export const logo_sample = "https://www.100starlings.com/img/client-collectplus.jpg";
+export const e_commerce =
+	"https://images.unsplash.com/photo-1586880244406-556ebe35f282?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
+export const health_tech =
+	"https://images.unsplash.com/photo-1640622659208-98d70201f2da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80";
+export const finTech =
+	"https://images.unsplash.com/photo-1569025743873-ea3a9ade89f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80";
+export const web_development =
+	"https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80";
+export const app_development =
+	"https://images.unsplash.com/photo-1595675024853-0f3ec9098ac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
+export const web_design =
+	"https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80";
+export const infrastructure_design =
+	"https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80";
+export const iot_image =
+	"https://images.unsplash.com/photo-1626335507832-40da3b65de57?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370";
+export const it_consulting =
+	"https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80";
+export const startup_img =
+	"https://images.unsplash.com/photo-1523240795612-9a054b0db644?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370";
+export const charity_img =
+	"https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370";
+export const team_img = "/images/team_img.png";
+export const react_logo = "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg";
+export const aws_logo =
+	"https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg";
+export const aws_logo_white =
+	"https://sp-ao.shortpixel.ai/client/to_auto,q_lossy…wp-content/uploads/2020/04/AWS-Logo-halfwhite.png";
+export const elastic_logo = "https://cdn.worldvectorlogo.com/logos/elasticsearch.svg";
+export const js_logo =
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/640px-Unofficial_JavaScript_logo_2.svg.png";
+export const rails_logo =
+	"https://upload.wikimedia.org/wikipedia/commons/6/62/Ruby_On_Rails_Logo.svg";
+export const graphql_logo = "https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg";
+export const elixir_logo = "https://www.vectorlogo.zone/logos/elixir-lang/elixir-lang-icon.svg";
+export const nextjs_logo = "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg";
+export const postgresql_logo =
+	"https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg";
+export const mangodb_logo =
+	"https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/6CE17BE6-9858-405F-A44D8FFA2FDEE398/webimage-BC751595-FFFC-455A-98FDF8675708A3E5.png";
+export const clojure_logo = "https://upload.wikimedia.org/wikipedia/commons/5/5d/Clojure_logo.svg";
+export const ruby_logo = "https://upload.wikimedia.org/wikipedia/commons/7/73/Ruby_logo.svg";
+export const client_bbc = "https://www.100starlings.com/img/client-bbc.jpg";
